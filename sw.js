f28346780e8cf90cf5cff9a24c0a2f32c36d7612
lino paone <106866775+linopaone@users.noreply.github.com>
2026-09-13 20:59:55 +0200

    backupdata
    Backupdata optie toegevoegd

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'lincsafe-v5';
+const CACHE = 'lincsafe-v7';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png',
   './icon-192-maskable.png', './icon-512-maskable.png'];
 
