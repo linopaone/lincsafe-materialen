85c05743247f5907d06cfc864e33ff60c190c609
lino paone <106866775+linopaone@users.noreply.github.com>
2026-09-13 20:37:46 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'lincsafe-v4';
+const CACHE = 'lincsafe-v5';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png',
   './icon-192-maskable.png', './icon-512-maskable.png'];
 
